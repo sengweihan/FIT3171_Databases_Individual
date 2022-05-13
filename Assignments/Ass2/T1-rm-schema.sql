901c427dbff796988a8cd80e70505e0d9335b21d
SengWeiHan <wsen0004@student.monash.edu>
2022-05-13 23:18:37 +0800

    completed adding tables for team and entry together with its column comments and constraint as well as all foreign key constraint

--- Assignments/Ass2/T1-rm-schema.sql
+++ Assignments/Ass2/T1-rm-schema.sql
@@ -94,13 +94,116 @@ ALTER TABLE emercontact ADD CONSTRAINT emercontact_pk PRIMARY KEY(ec_phone);
     
 
 --ENTRY
+CREATE TABLE entry (
+    event_id         NUMBER(6) NOT NULL,
+    entry_no         NUMBER(5) NOT NULL,
+    entry_starttime  DATE,
+    entry_finishtime DATE,
+    comp_no          NUMBER(5) NOT NULL,
+    team_id          NUMBER(3),
+    char_id          NUMBER(3)
+);
 
+
+COMMENT ON COLUMN entry.event_id IS
+    'Event id (surrogate primary key)';
+
+COMMENT ON COLUMN entry.entry_no IS
+    'Entry number (unique for each event)';
+
+COMMENT ON COLUMN entry.entry_starttime IS
+    'The entrant start time';
+
+COMMENT ON COLUMN entry.entry_finishtime IS
+    'The entrant finish time';
+
+COMMENT ON COLUMN entry.comp_no IS
+    'Unique identifier for a competitor';
+
+COMMENT ON COLUMN entry.team_id IS
+    'Team identifier (unique)';
+
+COMMENT ON COLUMN entry.char_id IS
+    'Charity unique identifier';
+    
+
+ALTER TABLE entry ADD CONSTRAINT entry_pk PRIMARY KEY ( event_id,
+                                                        entry_no );
 
 
 --TEAM
+CREATE TABLE team (
+    team_id         NUMBER(3) NOT NULL,
+    team_name       VARCHAR2(30) NOT NULL,
+    carn_date       DATE NOT NULL,
+    team_no_members NUMBER(2) NOT NULL,
+    event_id        NUMBER(6) NOT NULL,
+    entry_no        NUMBER(5) NOT NULL,
+    char_id         NUMBER(3)
+);
 
+
+COMMENT ON COLUMN team.team_id IS
+    'Team identifier (unique)';
+
+COMMENT ON COLUMN team.team_name IS
+    'Team name';
+    
+COMMENT ON COLUMN team.carn_date IS
+    'Date of carnival (unique identifier)';
+
+COMMENT ON COLUMN team.team_no_members IS
+    'Number of team members';
+
+COMMENT ON COLUMN team.event_id IS
+    'Event id (surrogate primary key)';
+
+COMMENT ON COLUMN team.entry_no IS
+    'Entry number (unique for each event)';
+
+COMMENT ON COLUMN team.char_id IS
+    'Charity unique identifier';
+
+
+ALTER TAble team add CONSTRAINT TEAM_PK PRIMARY KEY ( TEAM_ID ) ;
+
+ALTER TABLE team ADD CONSTRAINT team_nk UNIQUE ( team_name,
+                                                 carn_date );
 
 
 -- Add all missing FK Constraints below here
-ALTER TABLE competitor ADD CONSTRAINT emercontact_competitor_fk FOREIGN KEY (ec_phone) REFERENCES emercontact (ec_phone);
+ALTER TABLE competitor
+    ADD CONSTRAINT emercontact_competitor_fk FOREIGN KEY ( ec_phone )
+        REFERENCES emercontact ( ec_phone );
 
+ALTER TABLE entry
+    ADD CONSTRAINT competitor_entry_fk FOREIGN KEY ( comp_no )
+        REFERENCES competitor ( comp_no );
+
+ALTER TABLE entry
+    ADD CONSTRAINT event_entry_fk FOREIGN KEY ( event_id )
+        REFERENCES event ( event_id );
+
+ALTER TABLE entry
+    ADD CONSTRAINT charity_entry_fk FOREIGN KEY ( char_id )
+        REFERENCES charity ( char_id );
+
+ALTER TABLE entry
+    ADD CONSTRAINT team_entry_fk FOREIGN KEY ( team_id )
+        REFERENCES team ( team_id );
+
+ALTER TABLE team
+    ADD CONSTRAINT carnival_team_fk FOREIGN KEY ( carn_date )
+        REFERENCES carnival ( carn_date );
+
+ALTER TABLE team
+    ADD CONSTRAINT charity_team_fk FOREIGN KEY ( char_id )
+        REFERENCES charity ( char_id );
+
+ALTER TABLE team
+    ADD CONSTRAINT entry_team_fk FOREIGN KEY ( entry_no )
+        REFERENCES entry ( entry_no );
+
+ALTER TABLE team
+    ADD CONSTRAINT entry_team_fk FOREIGN KEY ( event_id )
+        REFERENCES entry ( event_id );
