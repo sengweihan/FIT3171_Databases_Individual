61c57d079d15c57d133f5a246d64cdb34bd8ab33
SengWeiHan <wsen0004@student.monash.edu>
2022-05-14 19:12:45 +0800

    Inserted data for entry tables

--- Assignments/Ass2/T2-rm-insert.sql
+++ Assignments/Ass2/T2-rm-insert.sql
@@ -240,10 +240,322 @@ INSERT INTO competitor VALUES (
 );
 -- =======================================
 -- ENTRY
+-- Included at least 10 competitors
+-- Included at least 6 events from 3 different carnivals
+--  Have at least 5 competitors who join more than 2 events
+--  Have at least 2 uncompleted entries (registration only)
 -- =======================================
+INSERT INTO entry VALUES (
+    1,
+    1,
+    TO_DATE('07:00', 'HH:MI'),
+    TO_DATE('08:30', 'HH:MI'),
+    1,
+    NULL,
+    NULL
+);
 
+
+INSERT INTO entry VALUES (
+    1,
+    2,
+    TO_DATE('07:00', 'HH:MI'),
+    TO_DATE('08:30', 'HH:MI'),
+    2,
+    NULL,
+    3
+);
+
+INSERT INTO entry VALUES (
+    1,
+    3,
+    TO_DATE('07:00', 'HH:MI'),
+    TO_DATE('08:30', 'HH:MI'),
+    3,
+    NULL,
+    2
+);
+
+INSERT INTO entry VALUES (
+    2,
+    1,
+    TO_DATE('06:30', 'HH:MI'),
+    TO_DATE('07:30', 'HH:MI'),
+    4,
+    NULL,
+    4
+);
+
+INSERT INTO entry VALUES (
+    2,
+    2,
+    TO_DATE('06:30', 'HH:MI'),
+    TO_DATE('07:30', 'HH:MI'),
+    5,
+    NULL,
+    NULL
+);
+
+INSERT INTO entry VALUES (
+    2,
+    3,
+    TO_DATE('06:30', 'HH:MI'),
+    TO_DATE('07:30', 'HH:MI'),
+    6,
+    NULL,
+    1
+);
+
+INSERT INTO entry VALUES (
+    3,
+    1,
+    TO_DATE('07:30', 'HH:MI'),
+    TO_DATE('08:30', 'HH:MI'),
+    1,
+    NULL,
+    4
+);
+
+INSERT INTO entry VALUES (
+    3,
+    2,
+    TO_DATE('07:30', 'HH:MI'),
+    TO_DATE('08:30', 'HH:MI'),
+    2,
+    NULL,
+    2
+);
+
+INSERT INTO entry VALUES (
+    3,
+    3,
+    TO_DATE('07:30', 'HH:MI'),
+    TO_DATE('08:30', 'HH:MI'),
+    3,
+    NULL,
+    NULL
+);
+
+INSERT INTO entry VALUES (
+    4,
+    1,
+    TO_DATE('06:30', 'HH:MI'),
+    TO_DATE('07:30', 'HH:MI'),
+    4,
+    NULL,
+    2
+);
+
+INSERT INTO entry VALUES (
+    4,
+    2,
+    TO_DATE('06:30', 'HH:MI'),
+    TO_DATE('07:30', 'HH:MI'),
+    5,
+    NULL,
+    3
+);
+
+INSERT INTO entry VALUES (
+    4,
+    3,
+    TO_DATE('06:30', 'HH:MI'),
+    TO_DATE('07:30', 'HH:MI'),
+    7,
+    NULL,
+    2
+);
+
+INSERT INTO entry VALUES (
+    5,
+    1,
+    TO_DATE('06:00', 'HH:MI'),
+    TO_DATE('07:00', 'HH:MI'),
+    6,
+    NULL,
+    3
+);
+
+INSERT INTO entry VALUES (
+    5,
+    2,
+    TO_DATE('06:00', 'HH:MI'),
+    TO_DATE('07:00', 'HH:MI'),
+    8,
+    NULL,
+    1
+);
+
+INSERT INTO entry VALUES (
+    5,
+    3,
+    TO_DATE('06:00', 'HH:MI'),
+    TO_DATE('07:00', 'HH:MI'),
+    9,
+    NULL,
+    4
+);
+
+INSERT INTO entry VALUES (
+    6,
+    1,
+    TO_DATE('07:00', 'HH:MI'),
+    TO_DATE('08:00', 'HH:MI'),
+    1,
+    NULL,
+    3
+);
+
+INSERT INTO entry VALUES (
+    6,
+    2,
+    TO_DATE('07:00', 'HH:MI'),
+    TO_DATE('08:00', 'HH:MI'),
+    2,
+    NULL,
+    1
+);
+
+INSERT INTO entry VALUES (
+    6,
+    3,
+    TO_DATE('07:00', 'HH:MI'),
+    TO_DATE('08:00', 'HH:MI'),
+    3,
+    NULL,
+    4
+);
+
+
+INSERT INTO entry VALUES (
+    7,
+    1,
+    TO_DATE('07:00', 'HH:MI'),
+    TO_DATE('08:00', 'HH:MI'),
+    4,
+    NULL,
+    NULL
+);
+
+INSERT INTO entry VALUES (
+    7,
+    2,
+    TO_DATE('07:00', 'HH:MI'),
+    TO_DATE('08:00', 'HH:MI'),
+    5,
+    NULL,
+    1
+);
+
+
+INSERT INTO entry VALUES (
+    8,
+    1,
+    NULL,
+    NULL,
+    10,
+    NULL,
+    2
+);
+
+
+INSERT INTO entry VALUES (
+    8,
+    2,
+    NULL,
+    NULL,
+    11,
+    NULL,
+    1
+);
+
+INSERT INTO entry VALUES (
+    9,
+    1,
+    NULL,
+    NULL,
+    12,
+    NULL,
+    2
+);
+
+INSERT INTO entry VALUES (
+    10,
+    1,
+    TO_DATE('06:30','HH:MI'),
+    TO_DATE('07:30','HH:MI'),
+    13,
+    NULL,
+    NULL
+);
+
+
+INSERT INTO entry VALUES (
+    10,
+    2,
+    TO_DATE('06:30','HH:MI'),
+    TO_DATE('07:30','HH:MI'),
+    15,
+    NULL,
+    1
+);
+
+INSERT INTO entry VALUES (
+    11,
+    1,
+    TO_DATE('06:00','HH:MI'),
+    TO_DATE('07:00','HH:MI'),
+    14,
+    NULL,
+    3
+);
+
+
+
+INSERT INTO entry VALUES (
+    12,
+    1,
+    TO_DATE('06:00','HH:MI'),
+    TO_DATE('07:00','HH:MI'),
+    2,
+    NULL,
+    1
+);
+
+INSERT INTO entry VALUES (
+    13,
+    1,
+    TO_DATE('06:00','HH:MI'),
+    TO_DATE('07:45','HH:MI'),
+    5,
+    NULL,
+    2
+);
+
+INSERT INTO entry VALUES (
+    13,
+    2,
+    TO_DATE('06:00','HH:MI'),
+    TO_DATE('07:45','HH:MI'),
+    2,
+    NULL,
+    1
+);
+
+
+INSERT INTO entry VALUES (
+    14,
+    1,
+    NULL,
+    NULL,
+    10,
+    NULL,
+    1
+);
 
 -- =======================================
 -- TEAM
+-- Have at least 2 teams with more than 2 members
+-- At least one team with the same name in different carnivals
 -- =======================================
 
