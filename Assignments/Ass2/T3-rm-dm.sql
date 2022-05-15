b29a782af79756e7ea3cba2c753ad436bb0878b1
SengWeiHan <wsen0004@student.monash.edu>
2022-05-15 19:14:28 +0800

    Done with 3d

--- Assignments/Ass2/T3-rm-dm.sql
+++ Assignments/Ass2/T3-rm-dm.sql
@@ -32,43 +32,45 @@ INSERT INTO emercontact VALUES (
 );
 
 INSERT INTO competitor VALUES (
-    competitor_seq.nextval,
+    competitor_seq.NEXTVAL,
     'Daniel',
     'Kai',
     'M',
-    TO_DATE('22-Nov-1999','dd-Mon-yyyy'),
+    TO_DATE('22-Nov-1999', 'dd-Mon-yyyy'),
     'DanielKai@gmail.com',
     'Y',
     '0353603775',
     'P',
-    ( SELECT
-    ec_phone
-FROM
-    emercontact
-WHERE
-        ec_fname = 'Jack'
-    AND ec_lname = 'Kai'
-)
+    (
+        SELECT
+            ec_phone
+        FROM
+            emercontact
+        WHERE
+                ec_fname = 'Jack'
+            AND ec_lname = 'Kai'
+    )
 );
 
 INSERT INTO competitor VALUES (
-    competitor_seq.nextval,
+    competitor_seq.NEXTVAL,
     'Annabelle',
     'Kai',
     'F',
-    TO_DATE('18-May-2000','dd-Mon-yyyy'),
+    TO_DATE('18-May-2000', 'dd-Mon-yyyy'),
     'AnnabelleKai@gmail.com',
     'Y',
     '0882903359',
     'P',
-    ( SELECT
-    ec_phone
-FROM
-    emercontact
-WHERE
-        ec_fname = 'Jack'
-    AND ec_lname = 'Kai'
-)
+    (
+        SELECT
+            ec_phone
+        FROM
+            emercontact
+        WHERE
+                ec_fname = 'Jack'
+            AND ec_lname = 'Kai'
+    )
 );
 
 INSERT INTO entry VALUES (
@@ -156,8 +158,6 @@ INSERT INTO entry VALUES (
             char_name = 'Beyond Blue'
     )
 );
-
-
 
 INSERT INTO entry VALUES (
     (
@@ -305,12 +305,148 @@ INSERT INTO team VALUES (
     )
 );
 
-
-UPDATE entry SET team_id = (SELECT team_id FROM team WHERE team_name = 'Kai Speedstars') 
-WHERE comp_no = (SELECT comp_no FROM competitor WHERE comp_fname = 'Annabelle' AND comp_lname = 'Kai');
+UPDATE entry
+SET
+    team_id = (
+        SELECT
+            team_id
+        FROM
+            team
+        WHERE
+            team_name = 'Kai Speedstars'
+    )
+WHERE
+    comp_no = (
+        SELECT
+            comp_no
+        FROM
+            competitor
+        WHERE
+                comp_fname = 'Annabelle'
+            AND comp_lname = 'Kai'
+    );
 
 COMMIT;
 --3(d)
 
+UPDATE entry
+SET
+    event_id = (
+        SELECT
+            event_id
+        FROM
+            event
+        WHERE
+                eventtype_code = (
+                    SELECT
+                        eventtype_code
+                    FROM
+                        eventtype
+                    WHERE
+                        eventtype_desc = '10 Km Run'
+                )
+            AND carn_date = (
+                SELECT
+                    carn_date
+                FROM
+                    carnival
+                WHERE
+                    carn_name = 'RM Autumn Series Caulfield 2022'
+            )
+    ),
+    entry_no = (
+        SELECT
+            MAX(entry_no) + 1
+        FROM
+            entry
+        WHERE
+            event_id = (
+                SELECT
+                    event_id
+                FROM
+                    event
+                WHERE
+                        eventtype_code = (
+                            SELECT
+                                eventtype_code
+                            FROM
+                                eventtype
+                            WHERE
+                                eventtype_desc = '10 Km Run'
+                        )
+                    AND carn_date = (
+                        SELECT
+                            carn_date
+                        FROM
+                            carnival
+                        WHERE
+                            carn_name = 'RM Autumn Series Caulfield 2022'
+                    )
+            )
+    ),
+    team_id = (
+        SELECT
+            team_id
+        FROM
+            team
+        WHERE
+                team_name = 'Kai Speedstars'
+            AND carn_date = (
+                SELECT
+                    carn_date
+                FROM
+                    carnival
+                WHERE
+                    carn_name = 'RM Autumn Series Caulfield 2022'
+            )
+    )
+WHERE
+    comp_no = (
+        SELECT
+            comp_no
+        FROM
+            competitor
+        WHERE
+                comp_fname = 'Daniel'
+            AND comp_lname = 'Kai'
+    );
 
+
+UPDATE team
+SET
+    team_no_members = (
+        SELECT
+            COUNT(team_id)
+        FROM
+            entry
+        WHERE
+            team_id = (
+                SELECT
+                    team_id
+                FROM
+                    team
+                WHERE
+                        team_name = 'Kai Speedstars'
+                    AND carn_date = (
+                        SELECT
+                            carn_date
+                        FROM
+                            carnival
+                        WHERE
+                            carn_name = 'RM Autumn Series Caulfield 2022'
+                    )
+            )
+    )
+WHERE
+        team_name = 'Kai Speedstars'
+    AND carn_date = (
+        SELECT
+            carn_date
+        FROM
+            carnival
+        WHERE
+            carn_name = 'RM Autumn Series Caulfield 2022'
+    );
+
+COMMIT;
 --3(e)
