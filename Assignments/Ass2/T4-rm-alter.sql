4bbf86433fb3f554f91aaec08be8db597d868062
SengWeiHan <wsen0004@student.monash.edu>
2022-05-16 17:01:55 +0800

    Doing 4a

--- Assignments/Ass2/T4-rm-alter.sql
+++ Assignments/Ass2/T4-rm-alter.sql
@@ -1,10 +1,10 @@
 --****PLEASE ENTER YOUR DETAILS BELOW****
 --T4-rm-alter.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Applied Class No:
+--Student ID: 32229070
+--Student Name: SENG WEI HAN 
+--Unit Code: FIT3171
+--Applied Class No: TUTORIAL 7
 
 /* Comments for your marker:
 
@@ -15,8 +15,15 @@
 
 --4(a)
 
+ALTER TABLE entry ADD entry_elapsed_time NUMBER(5,2) DEFAULT NULL;
+
+COMMENT ON COLUMN entry.entry_elapsed_time IS 'runner''s elapsed time in an event';
+
+
 
 --4(b)
+
+
 
         
 --4(c)
