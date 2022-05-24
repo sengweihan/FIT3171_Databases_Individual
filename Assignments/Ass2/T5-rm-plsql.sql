cbb7457221b419763d35b416b882b557e4eaadd4
SengWeiHan <wsen0004@student.monash.edu>
2022-05-24 22:19:22 +0800

    Done with 5b and 5c as well as the test harness

--- Assignments/Ass2/T5-rm-plsql.sql
+++ Assignments/Ass2/T5-rm-plsql.sql
@@ -118,15 +118,7 @@ DECLARE
 BEGIN
 
     SELECT eventtype_code INTO eventtypecode FROM event WHERE event_id = :new.event_id;
-    
-   -- SELECT TRUNC((:new.entry_finishtime - :old.entry_starttime)*1440,2) INTO elapsedtime 
-    --FROM event JOIN entry ON event.event_id = entry.event_id WHERE entry.event_id = :old.event_id AND entry_no = :old.entry_no;
-    
-    --SELECT :old.comp_no INTO competitor
-    --FROM event JOIN entry ON event.event_id = entry.event_id WHERE entry.event_id = :old.event_id AND entry_no = :old.entry_no;
-    
-    --SELECT TRUNC((:new.entry_finishtime - :new.entry_starttime)*1440,2) INTO elapsedtime FROM entry
-    -- WHERE event_id = :old.event_id AND entry_no = :old.entry_no;
+
     
     IF (:new.entry_finishtime IS NOT NULL) THEN
         elapsedtime := TRUNC((:new.entry_finishtime - :new.entry_starttime)*1440,2);
@@ -136,22 +128,24 @@ BEGIN
         competitor := :new.comp_no;
     END IF;
     
+   -- Here we are changing the :new value not directly writing to the table
+   -- via say an update which would cause a mutating table error
     :new.entry_elapsed_time := elapsedtime;
     
-   -- SELECT comp_no INTO competitor FROM entry
-    --WHERE event_id = :old.event_id AND entry_no = :old.entry_no;
-    
-   -- UPDATE entry SET entry_elapsed_time = elapsedtime WHERE event_id = :old.event_id AND entry_no = :old.entry_no;
     
     SELECT eventtype_record INTO currenttime FROM eventtype WHERE eventtype_code = eventtypecode;
     
-    IF (currenttime IS NULL ) THEN 
-    UPDATE eventtype SET eventtype_record = elapsedtime , eventtype_recordholder = competitor
-    WHERE eventtype_code = eventtypecode;
+    IF ( currenttime IS NULL ) THEN
+    UPDATE eventtype
+    SET
+        eventtype_record = elapsedtime,
+        eventtype_recordholder = competitor
+    WHERE
+        eventtype_code = eventtypecode;
     
     ELSIF (currenttime IS NOT NULL AND elapsedtime < currenttime) THEN 
-    UPDATE eventtype SET eventtype_record = elapsedtime , eventtype_recordholder = competitor
-    WHERE eventtype_code = eventtypecode;
+        UPDATE eventtype SET eventtype_record = elapsedtime , eventtype_recordholder = competitor
+            WHERE eventtype_code = eventtypecode;
     
     END IF;
     
@@ -162,6 +156,7 @@ END;
 -- Test harness for 5(b)
 SELECT * FROM entry;
 
+-- FIRST COMPETITOR 
 INSERT INTO entry VALUES (
     14,
     4,
@@ -175,23 +170,200 @@ INSERT INTO entry VALUES (
 
 SELECT * FROM entry;
 
+-- TEST TRIGGER 
+-- TEST UPDATE
 UPDATE entry SET entry_finishtime = TO_DATE('11:35:30', 'HH24:MI:SS'), entry_starttime = TO_DATE('08:30:10', 'HH24:MI:SS')
 WHERE event_id = 14 AND entry_no =4 AND comp_no =1;
 
 select * from entry;
 
+-- SECOND COMPETITOR
 INSERT INTO entry VALUES (
     14,
-    4,
+    5,
     NULL,
     NULL,
-    2,
+    3,
     NULL,
     NULL,
     NULL
 );
+
+select * from entry;
+
+-- TEST TRIGGER
+-- TEST UPDATE
+UPDATE entry SET entry_finishtime = TO_DATE('11:10:30', 'HH24:MI:SS'), entry_starttime = TO_DATE('08:30:10', 'HH24:MI:SS')
+WHERE event_id = 14 AND entry_no =5 AND comp_no =3;
+
+select * from entry;
+
 rollback;
+
 --5(c)
+CREATE OR REPLACE PROCEDURE event_registration(
+    in_comp_no        IN NUMBER,
+    in_carn_date      IN DATE,
+    in_eventtype_desc IN VARCHAR2,
+    in_team_name      IN VARCHAR2,
+    out_message       OUT VARCHAR2
+)AS
+    valid_carn_date NUMBER;
+    valid_event_name NUMBER;
+    valid_team_name NUMBER;
+    entryno  NUMBER;
+    eventid  NUMBER;
+
+BEGIN 
+    SELECT COUNT(*) INTO valid_carn_date FROM carnival WHERE carn_date = in_carn_date;
+    
+    SELECT COUNT(*) INTO valid_event_name FROM eventtype WHERE eventtype_desc = in_eventtype_desc;
+    
+    IF ( valid_carn_date = 0 OR valid_event_name =  0 ) THEN 
+        out_message := 'carnival date and event name must be valid';
+    ELSE 
+        SELECT event_id INTO eventid FROM event WHERE carn_date = in_carn_date AND eventtype_code = (SELECT eventtype_code FROM 
+        eventtype WHERE eventtype_desc = in_eventtype_desc);
+    
+        SELECT entry_no INTO entryno FROM entry WHERE event_id = eventid AND comp_no = in_comp_no;
+        
+       IF (in_team_name IS NOT NULL) THEN 
+        SELECT COUNT(*) INTO valid_team_name FROM team WHERE team_name = in_team_name AND carn_date = in_carn_date;
+        
+        IF ( valid_team_name = 0 ) THEN 
+            INSERT INTO team VALUES(
+                team_seq.NEXTVAL,
+                in_team_name,
+                in_carn_date,
+                1,
+                eventid,
+                entryno
+                );
+                
+            out_message := 'A team record has been inserted';
+            
+        ELSE
+            -- update the entry's team id for that competitor into the existing team 
+            -- increase the number of the team by 1 since a new competitor had joined the team.
+            UPDATE entry SET team_id = (SELECT team_id FROM team WHERE team_name = in_team_name 
+            AND carn_date = in_carn_date) WHERE event_id = eventid AND entry_no = entryno;
+            
+            UPDATE team SET team_no_members = (SELECT team_no_members FROM team WHERE team_name = in_team_name
+            AND carn_date = in_carn_date) + 1 WHERE team_name = in_team_name AND carn_date = in_carn_date;
+            
+            out_message := 'A competitor''s team id has been updated in the entry table as well as number of 
+            members in the team table';
+            
+        END IF ;
+    ELSE 
+        out_message := 'A team name is not being provided';
+        
+    END IF;
+    
+END IF;
+
+END event_registration;
+/
+           
 
 -- Test Harness for 5(c)
 
+-- INITIAL DATA
+SELECT * FROM entry; 
+
+-- INSERT 2 COMPETITOR FOR TESTING PURPOSES
+INSERT INTO entry VALUES (
+    14,
+    7,
+    NULL,
+    NULL,
+    11,
+    NULL,
+    NULL,
+    NULL
+);
+
+INSERT INTO entry VALUES (
+    14,
+    10,
+    NULL,
+    NULL,
+    12,
+    NULL,
+    NULL,
+    NULL
+);
+
+select * from entry;
+
+-- EXECUTE PROCEDURE
+DECLARE 
+    output VARCHAR2(200);
+BEGIN
+    -- call the procedure with valid carnival date but invalid event type desc;
+    -- FAILED 
+    event_registration(11,TO_DATE('29/May/2022','DD/MON/YYYY'),'Run Power Rangers','Titans',output);
+    dbms_output.put_line(output);
+END;
+/
+
+-- EXECUTE PROCEDURE
+DECLARE 
+    output VARCHAR2(200);
+BEGIN
+    -- call the procedure with valid event name but invalid carnival date;
+    -- FAILED 
+    event_registration(11,TO_DATE('30/May/2022','DD/MON/YYYY'),'21.1 Km Half Marathon','Titans',output);
+    dbms_output.put_line(output);
+END;
+/
+
+-- EXECUTE PROCEDURE
+DECLARE 
+    output VARCHAR2(200);
+BEGIN
+    -- call the procedure with both invalid event name and carnival date;
+    -- FAILED 
+    event_registration(11,TO_DATE('30/May/2022','DD/MON/YYYY'),'Power Rangers','Titans',output);
+    dbms_output.put_line(output);
+END;
+/
+
+-- EXECUTE PROCEDURE
+DECLARE 
+    output VARCHAR2(200);
+BEGIN
+    -- call the procedure with no team name ;
+    -- FAILED 
+    event_registration(11,TO_DATE('29/May/2022','DD/MON/YYYY'),'21.1 Km Half Marathon',NULL,output);
+    dbms_output.put_line(output);
+END;
+/
+
+-- EXECUTE PROCEDURE
+DECLARE 
+    output VARCHAR2(200);
+BEGIN
+    -- call the procedure with a team name but does not exist yet in this carnival ;
+    -- PASSED
+    event_registration(11,TO_DATE('29/May/2022','DD/MON/YYYY'),'21.1 Km Half Marathon','Databases Runner',output);
+    dbms_output.put_line(output);
+END;
+/
+
+-- EXECUTE PROCEDURE
+DECLARE 
+    output VARCHAR2(200);
+BEGIN
+    -- call the procedure with a team name that is exist in this carnival ;
+    -- PASSED
+    event_registration(12,TO_DATE('29/May/2022','DD/MON/YYYY'),'21.1 Km Half Marathon','Giants',output);
+    dbms_output.put_line(output);
+END;
+/
+
+SELECT * from entry;
+
+rollback;
+
+    
